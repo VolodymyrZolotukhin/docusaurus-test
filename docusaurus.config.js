df6cd87c0e0c96c329f68fbfa2c8d1950fd348f2
Volodymyr Zolotukhin <zolotukhinvolodymyr@gmail.com>
2023-09-21 16:19:43 +0300

    CI edited

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://VolodymyrZolotukhin.github.io',
+  url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Projecting_of_information_technologies/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'VolodymyrZolotukhin', // Usually your GitHub org/user name.
-  projectName: 'Projecting_of_information_technologies', // Usually your repo name.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
